213a303bdf6bdd4ffa2c1cbf26b2e4485b80788c
Amine Akamhi <akamhiamine077@gmail.com>
2024-10-07 00:32:08 +0200

    Challenge 1

---
+++ Challenge #1/main.js
@@ -0,0 +1,18 @@
+// 1 . create a boolean Variable called 'myBoolean'
+const myBoolean = true;
+// 2 . create a String Variable called 'myBoolean'
+const myString = 'hello world';
+// 3 . create a number Variable called 'firstNumber'
+const firstNumber = 20;
+// 4 . create a number Variable called 'secondNumber'
+var secondNumber = 40;
+// re-assign
+var secondNumber = 80;
+// 5 . create a array
+const myArray = [myBoolean, myString];
+// 6  . create a object
+let myObject = {
+    firstProperty: myArray,
+    sumProperty: firstNumber + secondNumber
+};
+
